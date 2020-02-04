bf956dc4d52f4876758b9581f921bc87d24458ba
Horki <ivanzvonimirhorvat@gmail.com>
2020-02-05 00:00:57 +0100

    added primitive text reader

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,13 @@
+// STD
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <iterator>
 
+// Emscripten
 #include <emscripten/bind.h>
 
+// OpenCV
 #include <opencv2/highgui.hpp>
 #include <opencv2/core.hpp>
 
@@ -10,9 +15,16 @@ void test() {
   std::cout << CV_MAJOR_VERSION << std::endl;
 }
 
+void print_file(const int & addr, const std::size_t & len) {
+    char * data = reinterpret_cast<char *>(addr);
+    std::copy(data, data + len, std::ostream_iterator<char>(std::cout, ""));
+    std::cout << std::endl;
+}
+
 int increment(int n) { return ++n; }
 
 EMSCRIPTEN_BINDINGS(my_module) {
   emscripten::function("test", &test);
   emscripten::function("incr", &increment);
+  emscripten::function("print_file", &print_file);
 }
--- static/index.js
+++ static/index.js
@@ -2,12 +2,30 @@ Module = {
     // Don't run main on page load
     noInitialRun: true,
     // Run custom function on page load
-    onRuntimeInitialized: () => {
-        console.log(window.Module.test());
-        console.log(window.Module.incr(101))
+    preInit: () => {
+        //
     },
-    // // Custom function to process stdout: keep in memory
-    // print: stdout => output.push(stdout),
-    // // Custom function to process stderr: keep in memory
-    // print: stderr => console.error(stderr),
+    onRuntimeInitialized: () => {
+        //
+    },
+    print: stdout => output.push(stdout),
+    // Custom function to process stderr: keep in memory
+    print: stderr => console.error(stderr),
 };
+
+document.getElementById('upload')
+    .addEventListener('change', function(event) {
+        let f = this.files[0];
+        let reader = new FileReader();
+        reader.onloadend = evt => {
+            const uint8_t_arr = new Uint8Array(evt.target.result);
+            const uint8_t_ptr = window.Module._malloc(uint8_t_arr.length);
+            window.Module.HEAPU8.set(uint8_t_arr, uint8_t_ptr);
+            window.Module.print_file(uint8_t_ptr, uint8_t_arr.length);
+            const returnArr = new Uint8Array(uint8_t_arr.length);
+            returnArr.set(window.Module.HEAPU8.subarray(uint8_t_ptr, uint8_t_ptr + uint8_t_arr.length));
+            window.Module._free(uint8_t_ptr);
+        };
+        reader.readAsArrayBuffer(f);
+    }
+);
