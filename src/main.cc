0cd68c21aee755634c89dde171332dcbd110c4f7
Horki <ivanzvonimirhorvat@gmail.com>
2020-02-15 18:36:38 +0100

    cc: added buffer val

--- src/main.cc
+++ src/main.cc
@@ -29,25 +29,39 @@ class LaneLines {
 private:
     cv::Mat img_original;
     cv::Mat img_current;
+    cv::Mat img_buffer;
 public:
     LaneLines(emscripten::val const & js_image) {
         convert_to_mat(js_image);
+        img_original.copyTo(img_current); // copy
     }
     ~LaneLines() {
         std::cout << "Here I will do some memory GC" << std::endl;
     }
-    Imaag to_gray() {
-        cv::cvtColor(img_original, img_current, cv::COLOR_RGBA2GRAY);
-        return to_imag();
+    void to_gray() {
+        cv::cvtColor(img_current, img_buffer, cv::COLOR_RGBA2GRAY);
     }
-    Imaag to_gaussian(int kernel, double sigma_x, double sigma_y) {
-        cv::GaussianBlur(img_current, img_current,
+    void to_gaussian(int kernel, double sigma_x, double sigma_y) {
+        cv::GaussianBlur(img_current, img_buffer,
                 cv::Size(kernel, kernel), sigma_x, sigma_y);
-        return to_imag();
     }
-    Imaag to_canny(double threshold_1, double threshold_2, int aperture) {
-        cv::Canny(img_current, img_current, threshold_1, threshold_2, aperture);
-        return to_imag();
+    void to_canny(double threshold_1, double threshold_2, int aperture) {
+        cv::Canny(img_current, img_buffer, threshold_1, threshold_2, aperture);
+    }
+    void to_next() {
+//        img_current = std::move(img_buffer);
+        img_current = img_buffer; // copy
+    }
+    Imaag to_imaag() {
+        // https://emscripten.org/docs/porting/emscripten-runtime-environment.html#emscripten-memory-model
+        int size = img_buffer.cols * img_buffer.rows * img_buffer.channels();
+        return {
+                img_buffer.cols,                       // Width
+                img_buffer.rows,                       // Height
+                img_buffer.channels(),                 // Channels
+                size,                                  // Size
+                reinterpret_cast<int>(img_buffer.data) // Pointer to image address
+        };
     }
 
 private:
@@ -61,17 +75,6 @@ private:
         std::uint8_t * destBuffer = img_original.data;
         emscripten::val memoryView(emscripten::typed_memory_view(length, destBuffer));
         memoryView.call<void>("set", imgData);
-    }
-    Imaag to_imag() {
-        // https://emscripten.org/docs/porting/emscripten-runtime-environment.html#emscripten-memory-model
-        int size = img_current.cols * img_current.rows * img_current.channels();
-        return {
-            img_current.cols,                       // Width
-            img_current.rows,                       // Height
-            img_current.channels(),                 // Channels
-            size,                                   // Size
-            reinterpret_cast<int>(img_current.data) // Pointer to image address
-        };
     }
 };
 
@@ -99,7 +102,9 @@ EMSCRIPTEN_BINDINGS(my_module) {
       .constructor<emscripten::val const &>()
       .function("to_gray",     &LaneLines::to_gray,     emscripten::allow_raw_pointers())
       .function("to_gaussian", &LaneLines::to_gaussian, emscripten::allow_raw_pointers())
-      .function("to_canny",    &LaneLines::to_canny,    emscripten::allow_raw_pointers());
+      .function("to_canny",    &LaneLines::to_canny,    emscripten::allow_raw_pointers())
+      .function("to_next",     &LaneLines::to_next,     emscripten::allow_raw_pointers())
+      .function("to_imaag",    &LaneLines::to_imaag,    emscripten::allow_raw_pointers());
   emscripten::value_object<Imaag>("Imaag")
     .field("width",    &Imaag::width)
     .field("height",   &Imaag::height)
