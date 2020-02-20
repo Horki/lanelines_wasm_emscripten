5f954b3687b252fb83f3348fb93f6311720c855b
Horki <ivanzvonimirhorvat@gmail.com>
2020-02-20 18:02:47 +0100

    cc: add region of interest for canny

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,8 @@
 #include <string>
 #include <algorithm>
 #include <iterator>
+#include <vector>
+#include <cassert>
 
 // Emscripten
 #include <emscripten.h>
@@ -15,7 +17,7 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
-
+#include <opencv2/core.hpp>
 
 struct Imaag {
     int width;
@@ -35,6 +37,14 @@ public:
         convert_to_mat(js_image);
         img_original.copyTo(img_current); // copy
     }
+    LaneLines(const cv::Mat & img) {
+        img_original = img;
+        img_buffer   = img;
+    }
+    LaneLines(cv::Mat && img) {
+        img_original = std::move(img);
+        img_original.copyTo(img_buffer);
+    }
     ~LaneLines() {
         std::cout << "Here I will do some memory GC" << std::endl;
     }
@@ -50,6 +60,27 @@ public:
         img_buffer.release();
         cv::Canny(img_current, img_buffer,
                 threshold_1, threshold_2, aperture);
+    }
+    void to_region(size_t x_1, size_t y_1, size_t x_2, size_t y_2) {
+        img_buffer.release();
+        // https://docs.opencv.org/3.4/d3/d96/tutorial_basic_geometric_drawing.html
+        assert(x_1 <= img_current.cols);
+        assert(y_1 <= img_current.rows);
+        assert(x_2 <= img_current.cols);
+        assert(y_2 <= img_current.rows);
+        cv::Mat mask = cv::Mat::zeros(img_current.rows, img_current.cols, img_current.channels());
+        std::vector<std::vector<cv::Point>> points {{
+            cv::Point(0           , img_current.rows),
+            cv::Point(int(x_1)        , int(y_1)),
+            cv::Point(int(x_2)        , int(y_2)),
+            cv::Point(img_current.cols, img_current.rows)
+        }};
+        cv::fillPoly(mask, points, cv::Scalar(255, 255, 255));
+        // select from canny image by polygon
+        cv::bitwise_and(img_current, img_current, img_buffer, mask);
+    }
+    void to_houghes() {
+        // TODO: add later
     }
     void to_next() {
         img_buffer.copyTo(img_current);
@@ -105,6 +136,8 @@ EMSCRIPTEN_BINDINGS(my_module) {
       .function("to_gray",     &LaneLines::to_gray,     emscripten::allow_raw_pointers())
       .function("to_gaussian", &LaneLines::to_gaussian, emscripten::allow_raw_pointers())
       .function("to_canny",    &LaneLines::to_canny,    emscripten::allow_raw_pointers())
+      .function("to_region",   &LaneLines::to_region,   emscripten::allow_raw_pointers())
+      .function("to_houghes",  &LaneLines::to_houghes,  emscripten::allow_raw_pointers())
       .function("to_next",     &LaneLines::to_next,     emscripten::allow_raw_pointers())
       .function("to_imaag",    &LaneLines::to_imaag,    emscripten::allow_raw_pointers());
   emscripten::value_object<Imaag>("Imaag")
